fab6d8b6de308a409135cdbcc0dea65770111e71
lindexi <lindexi_gd@163.com>
2021-06-20 12:54:52 +0800

    加上单元测试

---
+++ tests/DotnetCampusP2PFileShare.Web.Tests/Controllers/ResourceControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http.Json;
+using DotnetCampusP2PFileShare.Model;
+using DotnetCampusP2PFileShare.Web.Tests.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTest.Extensions.Contracts;
+
+namespace DotnetCampusP2PFileShare.Web.Tests.Controllers
+{
+    [TestClass()]
+    public class ResourceControllerTests
+    {
+        [ContractTestCase]
+        public void DownloadTest()
+        {
+            "执行下载动作，可以返回成功执行下载动作".Test(async () =>
+            {
+                var testClient = TestHostBuild.GetTestClient();
+
+                var downloadFileInfo = new DownloadFileInfo()
+                {
+                    DownloadFolderPath = Path.GetTempPath(),
+                    FileId = Guid.NewGuid().ToString(),
+                    FileName = Path.GetRandomFileName(),
+                };
+
+                var result = await testClient.PostAsJsonAsync("api/Resource/Download", downloadFileInfo);
+
+                Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+            });
+        }
+    }
+}
---
+++ tests/DotnetCampusP2PFileShare.Web.Tests/Framework/TestHostBuild.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotnetCampusP2PFileShare.Web.Tests.Framework
+{
+    [TestClass]
+    public static class TestHostBuild
+    {
+        public static HttpClient GetTestClient() => _host.GetTestClient();
+
+        [AssemblyInitialize]
+        public static async Task GlobalInitialize(TestContext testContext)
+        {
+            IHost host = await CreateAndRun();
+            _host = host;
+        }
+
+        private static IHost _host;
+
+        [AssemblyCleanup]
+        public static void GlobalCleanup()
+        {
+            _host.Dispose();
+        }
+
+        private static Task<IHost> CreateAndRun() => CreateHostBuilder().StartAsync();
+
+        public static IHostBuilder CreateHostBuilder() =>
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseTestServer(); //关键是多了这一行建立TestServer
+                });
+    }
+}
